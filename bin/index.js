8ff926d19255da1c358b45fb0795c2e50713e0e2
John Story <johnstory@Johns-MacBook-Pro.local>
2016-06-10 18:57:08 -0500

    skeleton of logic started

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,40 @@
-console.log('hello world');
+const inquirer = require('inquirer');
+
+let character = {};
+let game = require('./../game.json');
+const rooms = game.rooms;
+
+const hasName = game.name || false;
+const hasDescription = game.description || false;
+
+hasName? console.log(`Welcome to ${game.name}`) : null;
+hasDescription? console.log(game.description) : null;
+runMainLoop();
+function runMainLoop(room){
+    if (!room){
+        runMainLoop(getStartRoom());
+    } else {
+        inquirer.prompt([{
+            type : 'input',
+            name : 'val',
+            message : room.description
+        }])
+            .then(function(ans){
+                let args = ans.val.trim().split(' ');
+                console.log(args);
+            });
+    }
+}
+
+function getStartRoom(){
+    if(game.start){
+        return getRoomByName(game.start);
+    }
+    return rooms[0] ;
+}
+
+function getRoomByName(name){
+    return rooms.filter((x)=>{
+        return x.name === name ;
+    })[0] ;
+}
